aa278fe699957c13fbe27397ec59f0e5a1523c75
samuelZ20 <frizzonecardoso@gmail.com>
2025-10-15 11:41:41 -0300

    feat: Adiciona configuração Docker e valida execução em contêiner

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-
-// Rota principal que responde com uma mensagem simples
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.status(200).send('Pipeline CI/CD Funcionando!');
 });
-
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+}
 module.exports = app;
