aa38e3cc188476ae4f1d1b69a33f6a6242acecf3
samuelZ20 <frizzonecardoso@gmail.com>
2025-11-10 23:19:18 -0300

    feat:Implementa nova mensagme na API

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.status(200).send('Olá FUNCIONOU!');
+  res.status(200).send('Olá membros Comp!');
 });
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
